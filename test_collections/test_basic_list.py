72a5899194b24b8fd7a9a7af8e13354c999f7602
chris <chris.geralds@gmail.com>
2020-06-20 16:06:41 -0500

    fail test

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,9 +1,12 @@
 import unittest
+from unittest.mock import patch
+import fun_with_collections.basic_list as basic
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
+    def test_make_list(self, input):
+        self.assertEqual(basic.make_list(),[4, 67, 90])
 
 
 if __name__ == '__main__':
